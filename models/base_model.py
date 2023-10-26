aa4be45371e2f7d3b489e8e6b49120f532c7eba9
MrKindness235 <patriciocoitino235@gmail.com>
2023-10-26 07:27:32 -0700

    This project is amazing! First step towards the AirB&B Clone!

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A base model for the AirB&B console."""
+
+
+from datetime import datetime
+from uuid import uuid4
+
+class BaseModel():
+    def __init__(self):
+        """Initializes the instance."""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Updates."""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns keys/values of __dict__."""
+        new_dict = self.__dict__.copy()
+        new_dict.update({"created_at": self.created_at.isoformat()})
+        new_dict.update({"updated_at": self.updated_at.isoformat()})
+        new_dict.update({"__class__": self.__class__.__name__})
+        return new_dict
+
+        
+
+    def __str__(self):
+        """Must print: [<class name>] (<self.id>) <self.__dict__>"""
+
+        print("{} {} {}".format(self.__class__, self.id, self.__dict__))
